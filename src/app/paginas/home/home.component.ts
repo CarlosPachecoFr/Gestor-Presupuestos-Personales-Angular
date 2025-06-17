92332e071b8195b4de1fd2d61dd5d9b1299f3cd0
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-17 12:34:25 +0200

    Estructura basica del componente barra de navegacion de la Home.

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -3,10 +3,11 @@ import { TarjetasDatosComponent } from "../../componentes/home/tarjetas-datos/ta
 import { FormularioTransaccionComponent } from "../../componentes/home/formulario-transaccion/formulario-transaccion.component";
 import { TransaccionesRecientesComponent } from "../../componentes/home/transacciones-recientes/transacciones-recientes.component";
 import { GraficosTransaccionesComponent } from "../../componentes/home/graficos-transacciones/graficos-transacciones.component";
+import { BarraNavegacionComponent } from "../../componentes/home/barra-navegacion/barra-navegacion.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TarjetasDatosComponent, FormularioTransaccionComponent, TransaccionesRecientesComponent, GraficosTransaccionesComponent],
+  imports: [TarjetasDatosComponent, FormularioTransaccionComponent, TransaccionesRecientesComponent, GraficosTransaccionesComponent, BarraNavegacionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

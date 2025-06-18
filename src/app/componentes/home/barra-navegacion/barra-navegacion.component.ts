85d43bd2c617b450278afa4bc91489fb0dbc24e4
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-18 11:22:50 +0200

    Estilos dinamicos para la barra de navegacion de la home, falta funcionalidad.

--- src/app/componentes/home/barra-navegacion/barra-navegacion.component.ts
+++ src/app/componentes/home/barra-navegacion/barra-navegacion.component.ts
@@ -1,11 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-barra-navegacion',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './barra-navegacion.component.html',
   styleUrl: './barra-navegacion.component.css'
 })
 export class BarraNavegacionComponent {
+  opcionMenu: string = 'dashboard';
 
+  constructor() { }
+
+  cambiarOPcionMenu(opcion: string) {
+    this.opcionMenu = opcion;
+    console.log('Opción de menú cambiada a:', this.opcionMenu);
+  }
 }

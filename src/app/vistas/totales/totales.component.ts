0b73761d8fe2c530e5a6cafb16195fb5041c837d
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-22 12:38:19 +0200

    Creada vista Totales.

---
+++ src/app/vistas/totales/totales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-totales',
+  imports: [],
+  templateUrl: './totales.component.html',
+  styleUrl: './totales.component.css'
+})
+export class TotalesComponent {
+
+}

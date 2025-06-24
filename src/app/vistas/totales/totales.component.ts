ebb8c4296d7dcdd5f9a023bb0e2f3ef05f688a9d
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-24 17:25:55 +0200

    Componente con informacion general en la vista Totales creado, falta darle funcionalidad.

--- src/app/vistas/totales/totales.component.ts
+++ src/app/vistas/totales/totales.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TarjetasDatosTotalesComponent } from "../../componentes/totales/tarjetas-datos-totales/tarjetas-datos-totales.component";
 import { GraficoTotalesComponent } from "../../componentes/totales/grafico-totales/grafico-totales.component";
+import { InformacionGeneralComponent } from "../../componentes/totales/informacion-general/informacion-general.component";
 
 @Component({
   selector: 'app-totales',
-  imports: [TarjetasDatosTotalesComponent, GraficoTotalesComponent],
+  imports: [TarjetasDatosTotalesComponent, GraficoTotalesComponent, InformacionGeneralComponent],
   templateUrl: './totales.component.html',
   styleUrl: './totales.component.css'
 })

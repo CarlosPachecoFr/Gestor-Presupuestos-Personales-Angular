c3033b08c1511f8d07eee31aec3b1a577618ad47
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-26 14:05:24 +0200

    Estructura basica de la tarjeta de una meta en la vista Metas.

--- src/app/componentes/metas/todo-metas/todo-metas.component.ts
+++ src/app/componentes/metas/todo-metas/todo-metas.component.ts
@@ -1,11 +1,13 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-metas',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './todo-metas.component.html',
   styleUrl: './todo-metas.component.css'
 })
 export class TodoMetasComponent {
 
+  porcentaje: number = 90;
 }

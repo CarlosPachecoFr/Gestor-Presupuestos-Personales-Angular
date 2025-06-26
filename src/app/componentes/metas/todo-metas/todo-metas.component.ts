6a65b10c283cc13a6d3259889bd2083160e2e603
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-26 16:32:45 +0200

    Añadida funcionalidad a las tarjetas de metas, ahora cargan los datos obtenidos del back. Falta darle funcionalidad al boton añadir dinero y meta.

--- src/app/componentes/metas/todo-metas/todo-metas.component.ts
+++ src/app/componentes/metas/todo-metas/todo-metas.component.ts
@@ -1,5 +1,6 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { MetaService } from '../../../services/meta.service';
 
 @Component({
   selector: 'app-todo-metas',
@@ -9,5 +10,59 @@ import { Component } from '@angular/core';
 })
 export class TodoMetasComponent {
 
-  porcentaje: number = 90;
+  metas: any = [];
+
+  constructor(private metaService: MetaService){}
+
+  ngOnInit(){
+    this.cargarDatos();
+  }
+
+  cargarDatos(){
+    this.metaService.obtenerMetasUsuarioId().subscribe(metas => {
+    this.metas = metas.map(meta => {
+      const porcentaje = meta.cantidad_objetivo > 0
+        ? parseFloat(((meta.cantidad_actual / meta.cantidad_objetivo) * 100).toFixed(2))
+        : 0;
+
+      const diferencia_objetivo = meta.cantidad_objetivo - meta.cantidad_actual;
+
+      const hoy = new Date();
+
+      const fechaFin = new Date(meta.fecha_finalizacion);
+
+      let tiempo_restante = '';
+
+      if (fechaFin < hoy) {
+        tiempo_restante = 'Vencido';
+      } else {
+          const diffTiempo = fechaFin.getTime() - hoy.getTime(); // milisegundos
+          const diffDias = Math.floor(diffTiempo / (1000 * 60 * 60 * 24));
+
+          const meses = Math.floor(diffDias / 30);
+          const dias = diffDias % 30;
+
+          if (meses > 0 && dias > 0) {
+            tiempo_restante = `Faltan ${meses} meses y ${dias} días`;
+          } else if (meses > 0) {
+            tiempo_restante = `Faltan ${meses} meses`;
+          } else if (dias > 0) {
+            tiempo_restante = `Faltan ${dias} días`;
+          } else {
+            tiempo_restante = 'Vence hoy';
+          }
+        }
+
+         const color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+
+      return {
+        ...meta,
+        porcentaje,
+        diferencia_objetivo,
+        tiempo_restante,
+        color
+      };
+    });
+  });
+  }
 }
--- src/app/services/meta.service.ts
+++ src/app/services/meta.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+
+export interface Meta {
+  id: number,
+  nombre: string,
+  cantidad_actual: number,
+  cantidad_objetivo: number,
+  fecha_finalizacion: Date,
+  usuario_id: number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetaService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl + '/metas';
+
+  constructor(private http: HttpClient) { }
+
+  crearMeta(meta: Meta){
+    const token = localStorage.getItem('token');
+    return this.http.post<Meta>(`${this.apiUrl}/crearMeta`, meta, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  obtenerMetasUsuarioId(){
+    const token = localStorage.getItem('token');
+    return this.http.get<Meta[]>(`${this.apiUrl}/obtenerMetasUsuarioId`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
 }

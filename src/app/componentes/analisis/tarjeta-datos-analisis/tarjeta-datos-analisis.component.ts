4ad083b833daee84bba5396d7b436709232fa564
CarlosPachecoFr <carlospf1717@gmail.com>
2025-06-30 12:17:02 +0200

    Añadida funcionalidad a las tarjetas de datos de la vista Analisis.

--- src/app/componentes/analisis/tarjeta-datos-analisis/tarjeta-datos-analisis.component.ts
+++ src/app/componentes/analisis/tarjeta-datos-analisis/tarjeta-datos-analisis.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Transaccion, TransaccionService } from '../../../services/transaccion.service';
 
 @Component({
   selector: 'app-tarjeta-datos-analisis',
@@ -8,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class TarjetaDatosAnalisisComponent {
 
+  tasaAhorro: number = 0;
+  patrimonioNeto: number = 0;
+  gastoDiarioPromedio: number = 0;
+  transaccionesFiltrar: any = [];
+  alertas: number = 0;
+
+  constructor(private transaccionService: TransaccionService){}
+
+  ngOnInit(){
+    this.cargarDatos();
+    
+    this.transaccionService.transaccionesActualizadas$.subscribe(() => {
+      this.cargarDatos();
+    });
+  }
+
+  cargarDatos(){
+    this.transaccionService.obtenerTasaAhorroPorId().subscribe(tasa => {
+      this.tasaAhorro = tasa;
+    });
+    this.transaccionService.obtenerGastosMensualPorId().subscribe(gasto => {
+      this.gastoDiarioPromedio = parseFloat((gasto/30).toFixed(2)); //entre 30 como aproximacion del mes actual
+    })
+    this.transaccionService.obtenerBalancePorId().subscribe(balance => {
+      this.patrimonioNeto = balance;
+    })
+    this.transaccionService.obtenerTransacciones().subscribe(transacciones => {
+      this.transaccionesFiltrar = transacciones;
+      this.alertas = this.transaccionesFiltrar.filter((t: Transaccion) => t.cantidad > 1000).length;
+    })
+  }
 }

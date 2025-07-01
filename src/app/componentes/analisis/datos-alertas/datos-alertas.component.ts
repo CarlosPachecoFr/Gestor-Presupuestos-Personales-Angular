8d60d7c6fa51dc731c9015ec57da21d4d033bc42
CarlosPachecoFr <carlospf1717@gmail.com>
2025-07-01 12:54:09 +0200

    Añadida funcionalidad a las tarjetas de alertas en la vista de Analisis.

--- src/app/componentes/analisis/datos-alertas/datos-alertas.component.ts
+++ src/app/componentes/analisis/datos-alertas/datos-alertas.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
+import { Transaccion, TransaccionService } from '../../../services/transaccion.service';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+
+registerLocaleData(localeEs);
 
 @Component({
   selector: 'app-datos-alertas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './datos-alertas.component.html',
-  styleUrl: './datos-alertas.component.css'
+  styleUrl: './datos-alertas.component.css',
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ]
 })
 export class DatosAlertasComponent {
 
+  transaccionesFiltradas: any = [];
+
+  constructor(private transaccionService: TransaccionService){}
+
+  ngOnInit(){
+    this.cargarDatos();
+    this.transaccionService.transaccionesActualizadas$.subscribe(() => {
+      this.cargarDatos();
+    })
+  }
+
+  cargarDatos(){
+    this.transaccionService.obtenerTransacciones().subscribe(transacciones => {
+      this.transaccionesFiltradas = transacciones.filter((t: Transaccion) => t.cantidad > 1000);
+    })
+  }
 }

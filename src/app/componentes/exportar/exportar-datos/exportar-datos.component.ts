6e4a36c4e51111bdda36e0d5dacd5cc0a5992e8e
CarlosPachecoFr <carlospf1717@gmail.com>
2025-07-02 13:22:02 +0200

    Estructura y estilos del componente para exportar datos terminado, falta darle funcionalidad.

--- src/app/componentes/exportar/exportar-datos/exportar-datos.component.ts
+++ src/app/componentes/exportar/exportar-datos/exportar-datos.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exportar-datos',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './exportar-datos.component.html',
   styleUrl: './exportar-datos.component.css'
 })
 export class ExportarDatosComponent {
 
+  formularioExportar: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.formularioExportar = this.formBuilder.group({
+      periodo: [''],
+      formato: ['']
+    })
+  }
 }
